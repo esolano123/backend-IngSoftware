d9894c989dcbf32b70647e5675e2ad2f205148af
Eloise Solano <eloise.solano@anahuacmayab.edu.mx>
2025-11-09 13:54:12 -0600

    Updated project files and fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log('hola Joshhhhhhhhh')
+//console.log('hola mundo')
+
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require(`./config/db`)
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+
+const port = process.env.PORT || 6000
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/tareas', require('./routes/tareasRoutes'))
+app.use('/api/users', require('./routes/usersRoutes'))
+app.use(errorHandler)
+
+
+
+app.listen(port, () => console.log(`Servidor iniciado en el puerto: ${port}`));
+

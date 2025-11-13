0e276567d220b5b1c765943b05e7b48117127189
Eloise Solano <eloise.solano@anahuacmayab.edu.mx>
2025-11-13 00:19:57 -0600

    Final

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,16 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require(`./config/db`)
 const {errorHandler} = require('./middleware/errorMiddleware')
+const cors = require('cors')
 
 
 const port = process.env.PORT || 6000
 connectDB()
 
 const app = express()
+
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
